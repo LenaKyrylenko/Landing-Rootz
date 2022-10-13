fa586feb665b356a768982e8dbfe8813f9802642
LenaKyrylenko <lena.kirilenko13@gmail.com>
2022-10-14 00:27:53 +0300

    Initialization project, add header and materials

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.scss';
+import Header from './components/Header';
+function App() {
+  return (
+    <div className="App">
+   <Header/>
+    </div>
+  );
+}
+
+export default App;
